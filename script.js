4d02d6de96a1835f414c209cf585f0d03dd5bf07
Saurav Kumar <saurav.kumar@juegostudio.net>
2019-10-09 15:25:39 +0530

    retirement calculation:

--- script.js
+++ script.js
@@ -1,40 +1,52 @@
-var name, age, isMarried;
-name = "saurav";
-age = 44;
-isMarried = false;
+// var value = prompt("Enter youre name");
 
-console.log(name)
-console.log(age)
-console.log("Type coercion(Dynamic typing)",name+age)
-console.log("variable mutation: changing   value of variable")
+// switch (value) {
+//   case "Rahul":
+//     console.log("Match found");
+//     break;
+//   case "Saurav":
+//     console.log("Match found");
+//     break;
+//   default:
+//     console.log("Match not found");
+// }
 
-// var enteredText = prompt("Enter some text");
-// alert("The entered text is "+enteredText)
+// var player1Height = prompt("Enter player1 Height");
+// var player1Age = prompt("Enter player1 Age");
 
-if(11 === "11"){
-    console.log("yes")
+// var player2Height = prompt("Enter player2 Height");
+// var player2Age = prompt("Enter player2 Age");
+
+// player1Result = player1Height + player1Age * 5;
+// player2Result = player2Height + player2Age * 5;
+
+// if (player1Result > player2Result) {
+//   console.log("Player1 Wins");
+// } else if (player1Result < player2Result) {
+//   console.log("player2 Wins");
+// }
+// else{
+//     console.log("Draw")
+// }
+
+var currentyear = 2019;
+
+function calculateAge(yearOfBirth) {
+  var age = currentyear - yearOfBirth;
+  return age;
 }
-else{
-    console.log("no")
+
+function calculateTimeSpan(name, yearOfBirth) {
+  var retirementAge = 60;
+  var age = calculateAge(yearOfBirth);
+  var difference = retirementAge - age;
+  if (difference > 0) {
+    console.log(name + " has " + difference + "years left for Retirement");
+  } else {
+    console.log("Its been "+ difference*-1+"years since "+ name +" Retired");
+  }
 }
 
-var age = 45;
-
-function test() {
-    console.log("fun called")
-    var age = 105;
-}
-
-var value = prompt("Enter youre name");
-
-switch(value){
-    case 'Rahul':
-        console.log("Match found");
-        break;
-    case 'Saurav':
-        console.log("Match found");
-        break;
-    default:
-        console.log("Match not found y");        
-}
- 
+calculateTimeSpan("Alies",1997)
+calculateTimeSpan("Bob",1957)
+calculateTimeSpan("John",1959)
