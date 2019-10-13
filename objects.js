c89c3b4f060181d4863dbd9ad356f1101bc5c044
Saurav <amsauravkumar@gmail.com>
2019-10-13 23:34:41 +0530

    inheritence consept in objects

--- objects.js
+++ objects.js
@@ -10,7 +10,12 @@ var student = function(firstName, lastName, year){
     this.firstName = firstName;
     this.lastName = lastName;
     this.year = year;
+
 }
+student.prototype.calculateAge = function(){
+    console.log(2019 - this.year)
+} 
 
 var bob = new student('bob', 'jan', 1997);
-console.log(bob)
+console.log(bob) 
+bob.calculateAge();
