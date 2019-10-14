9220fafb7cfd0653d55b82011f416a91a3630639
Saurav Kumar <saurav.kumar@juegostudio.net>
2019-10-14 10:23:58 +0530

    inheriting properties

--- objects.js
+++ objects.js
@@ -3,14 +3,20 @@ var student = {
     lastName: 'raj',
     year: 1997
 }
-
+var year = 1222;
 console.log(student)
 
 var student = function(firstName, lastName, year){
     this.firstName = firstName;
     this.lastName = lastName;
     this.year = year;
+    this.calculateAge = function(){
+        this.age = (2019-this.year)
+        console.log(this.age)
+    }
+
 }
 
 var bob = new student('bob', 'jan', 1997);
+bob.calculateAge();
 console.log(bob)
