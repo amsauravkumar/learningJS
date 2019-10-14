09a54d9172d169c49f4d29f89a4e9a9e2d960aea
Saurav <amsauravkumar@gmail.com>
2019-10-14 16:14:42 +0530

    function returning function

--- function.js
+++ function.js
@@ -1,33 +1,52 @@
 var dob = [1997, 1854, 1998, 2000];
 
-function claculateAge(dob, fn){
-    var age=[];
-    for (var i=0;i<dob.length;i++){
-        age.push(fn(dob[i]));
-    }
-    return age;
+function claculateAge(dob, fn) {
+  var age = [];
+  for (var i = 0; i < dob.length; i++) {
+    age.push(fn(dob[i]));
+  }
+  return age;
 }
 
-function fn(i){
-    return 2019-i;
+function fn(i) {
+  return 2019 - i;
 }
 
 var ages = claculateAge(dob, fn);
-console.log(ages)
+console.log(ages);
 
 // prctice 2
 
- var float = [12.33, 5.55, 6.82, 233.3];
+var float = [12.33, 5.55, 6.82, 233.3];
 
- function rounfOff(float){
-     var int = [];
-    for(var i=0;i<float.length;i++){
-        int.push(i);
-    }
-    return int;
- }
+function rounfOff(float) {
+  var int = [];
+  for (var i = 0; i < float.length; i++) {
+    int.push(i);
+  }
+  return int;
+}
 
+var roundedOff = rounfOff(float);
+console.log(roundedOff);
 
- var ppp = rounfOff(float)
+// function within another function
 
- console.log(ppp)
+function interviewQuestion(jobRole) {
+  if (jobRole === "teacher") {
+    return function(name) {
+      console.log("what subjects can you teach " + name);
+    };
+  } else if (jobRole === "programmer") {
+    return function(name) {
+      console.log("Programming language you know");
+    };
+  } else {
+    console.log("job profile doesnt match any");
+  }
+}
+
+var teacherQuestion = interviewQuestion("teacher");
+teacherQuestion("joly");
+teacherQuestion("roy");
+teacherQuestion("gyan");
