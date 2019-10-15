534eca9b9efa50e57b6e8b3b2aba23e4c4367364
Saurav <amsauravkumar@gmail.com>
2019-10-15 23:47:16 +0530

    added data to work with

---
+++ dummydata.js
@@ -0,0 +1,70 @@
+var tournamentData = [{
+    tournament_id: "2hj3286pqov1g1g59k2t2qcgm",
+    country: "England",
+    tournament_name: "FA Cup",
+    tournament_code: "FAC",
+    tournamentCalendar: [
+        {
+            calendar_id: "aaa",
+            calendar_name: "2019/2020",
+            start_date: "2019-08-09Z",
+            end_date: "2020-05-23Z",
+            active: "yes",
+            historical_data_available: true
+        },
+        {
+            calendar_id: "bbb",
+            calendar_name: "2018/2019",
+            start_date: "2018-08-10Z",
+            end_date: "2019-05-18Z",
+            active: "no",
+            historical_data_available: true
+        }
+    ],
+}, {
+    tournament_id: "2hj3286pqov1g1g59ck2t2qcgm",
+    country: "England",
+    tournament_name: "FA Cup",
+    tournament_code: "FAC",
+    tournamentCalendar: [
+        {
+            calendar_id: "ccc",
+            calendar_name: "2019/2020",
+            start_date: "2019-08-09Z",
+            end_date: "2020-05-23Z",
+            active: "yes",
+            historical_data_available: true
+        },
+        {
+            calendar_id: "ddd",
+            calendar_name: "2018/2019",
+            start_date: "2018-08-10Z",
+            end_date: "2019-05-18Z",
+            active: "no",
+            historical_data_available: true
+        }
+    ],
+}, {
+    tournament_id: "2hj3286pqov1g1g59k2t2qcgm",
+    country: "England",
+    tournament_name: "FA Cup",
+    tournament_code: "FAC",
+    tournamentCalendar: [
+        {
+            calendar_id: "eee",
+            calendar_name: "2019/2020",
+            start_date: "2019-08-09Z",
+            end_date: "2020-05-23Z",
+            active: "yes",
+            historical_data_available: true
+        },
+        {
+            calendar_id: "fff",
+            calendar_name: "2018/2019",
+            start_date: "2018-08-10Z",
+            end_date: "2019-05-18Z",
+            active: "no",
+            historical_data_available: true
+        }
+    ],
+}]
