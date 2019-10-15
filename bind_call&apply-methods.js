1d0072b5f28d564d09294ea03a7a5de27419fb85
Saurav <amsauravkumar@gmail.com>
2019-10-15 10:27:52 +0530

    bind and call methods on objects

--- bind_call&apply-methods.js
+++ bind_call&apply-methods.js
@@ -4,14 +4,15 @@ var roy = {
   name: "roy",
   age: 30,
   message: function(greeting, secretCode) {
-    console.log(this.name + ", "+greeting + " ure secret code is " + secretCode);
+    console.log(
+      this.name + ", " + greeting + " ure secret code is " + secretCode
+    );
   }
 };
 
-
 var kane = {
-    name : "kane"
-}
+  name: "kane"
+};
 
 roy.message("hello", 555);
 roy.message.call(kane, "Hi", 999);
@@ -19,4 +20,4 @@ roy.message.call(kane, "Hi", 999);
 // Bind methods
 
 var gateMessage = roy.message.bind(kane);
-gateMessage('yo', 1000)
+gateMessage("yo", 1000);
